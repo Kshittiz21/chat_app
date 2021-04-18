8b2f407f21435f559284a13e31c7b7e0815738d4
Kshittiz Bhardwaj <59140501+Kshittiz21@users.noreply.github.com>
2021-04-18 15:24:38 +0530

    comments added  in chat_screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -17,11 +17,22 @@ class ChatScreen extends StatelessWidget {
         child: Icon(Icons.add),
         onPressed: () async {
           await Firebase.initializeApp();
-          FirebaseFirestore.instance
+          // We also need to import firebase_core.dart file b/c 
+          // before using any firebase core products we need to 
+          // Firebase.initializeApp(); and it returns future.
+          FirebaseFirestore.instance // there is always one
+          // active instance of firebase managed on our behalf
+          // using which we can execute methods:
               .collection('chats/03W8nBQbWfr9JHX6YwxQ/messages')
-              .snapshots()
+              // this is the path to our collections
+              .snapshots() //It allows us to setup a listener 
+              // through the Firebase Flutter SDK to our Firebase
+              // Database. Whenever data changes, this listener is
+              // modified automatically
               .listen((data) {
+            // using listen we are listening to changes
             data.docs.forEach((document) {
+              // it executes on every document in our collection
               print(document['text']);
             });
             //print(data.docs[0]['text']);
