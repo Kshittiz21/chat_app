d8662f471697cbcc41273e0a0a799d442cc31d29
Kshittiz Bhardwaj <59140501+Kshittiz21@users.noreply.github.com>
2021-04-18 14:52:47 +0530

    connecting with Firebase SDK

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +12,21 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This Works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          await Firebase.initializeApp();
+          FirebaseFirestore.instance
+              .collection('chats/03W8nBQbWfr9JHX6YwxQ/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((document) {
+              print(document['text']);
+            });
+            //print(data.docs[0]['text']);
+          });
+        },
       ),
     );
   }
