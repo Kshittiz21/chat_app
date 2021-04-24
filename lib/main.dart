0aa80c1abc637e8683794f1cdc0de0d947493928
Kshittiz Bhardwaj <59140501+Kshittiz21@users.noreply.github.com>
2021-04-24 13:43:25 +0530

    Firebase.initializeApp solved & StreamBuilder used

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import './screens/chat_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+// We could have called firebase.initializeApp() here or in initState() inside 
+// the class where we are using Firebase but this is a better option because in
+// the other one, we will get an error screen for 1 sec bcoz the screen will
+// try to load without Firebase.initializeApp() called and hence error will occur
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat',
-      theme: ThemeData( 
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: ChatScreen(),
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,42 +3,53 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class ChatScreen extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works!'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          await Firebase.initializeApp();
-          // We also need to import firebase_core.dart file b/c 
-          // before using any firebase core products we need to 
-          // Firebase.initializeApp(); and it returns future.
-          FirebaseFirestore.instance // there is always one
-          // active instance of firebase managed on our behalf
-          // using which we can execute methods:
-              .collection('chats/03W8nBQbWfr9JHX6YwxQ/messages')
-              // this is the path to our collections
-              .snapshots() //It allows us to setup a listener 
-              // through the Firebase Flutter SDK to our Firebase
-              // Database. Whenever data changes, this listener is
-              // modified automatically
-              .listen((data) {
-            // using listen we are listening to changes
-            data.docs.forEach((document) {
-              // it executes on every document in our collection
-              print(document['text']);
-            });
-            //print(data.docs[0]['text']);
-          });
+    return Scaffold (
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance // there is always one
+            // active instance of firebase managed on our behalf
+            // using which we can execute methods:
+            .collection('chats/03W8nBQbWfr9JHX6YwxQ/messages')
+            // this is the path to our collections
+            .snapshots(), //It allows us to setup a listener
+        // through the Firebase Flutter SDK to our Firebase
+        // Database. Whenever data changes, this listener is
+        // modified automatically and we can listen using
+        // .listen() method
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
         },
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () //async
+              {
+            //  await Firebase.initializeApp();
+            // // We also need to import firebase_core.dart file b/c
+            // // before using any firebase core products we need to
+            // // Firebase.initializeApp(); and it returns future.
+            // FirebaseFirestore.instance // there is always one
+            //     // active instance of firebase managed on our behalf
+            //     // using which we can execute methods:
+            //     .collection('chats/03W8nBQbWfr9JHX6YwxQ/messages')
+            //     // this is the path to our collections
+            //     .snapshots();
+          }),
     );
   }
+
 }
